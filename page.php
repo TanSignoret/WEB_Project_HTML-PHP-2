338de0bf139c2c960359feb8838ac0780a4b22e8
TanSignoret <tanguysignoret@gmail.com>
2020-03-03 17:57:43 +0100

    juste les encours

--- page.php
+++ page.php
@@ -132,7 +132,7 @@
         for ($i = 1; $i < $lg; $i++) {
           $qqch = fgets($dataFile);
           echo ($qqch);
-          // if ($qqch === 'encours'){ ?>
+           if ($qqch === 'encours'){ ?>
             <article class="jobs">
               <h2><?php echo fgets($dataFile); //titre de l'annonce ?></h2>
               <h3><?php echo fgets($dataFile); //lieu de l'annonce ?></h3>
@@ -143,10 +143,10 @@
                 <p><?php echo fgets($dataFile); //description de l'annonce ?></p>
               </div>
             </article>
-          <?php /* } else {
+          <?php } else {
             for ($i=0; $i <= 6; $i++) {
               fgets($dataFile);
-            } */
+            }
           }
 
         fclose($dataFile);
